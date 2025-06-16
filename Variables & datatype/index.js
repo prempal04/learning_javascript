a576f5a7ea03fe008a83d36259aa644efa500c72
Prempal <prempal.bt23ece@pec.edu.in>
2025-06-16 20:04:22 +0530

    learned about var and datatypes

---
+++ Variables & datatype/index.js
@@ -0,0 +1,69 @@
+// << Variable and Datatypes >>
+// variables are named memory locations
+
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// solve();
+
+//it's not a good practice to use same var more than one time;
+
+//well it reassignment and redefinement is possible in var;
+
+// var is function scoped or gloabally scoped not block scoped;
+// var x=20;
+// var x=10;
+
+
+//let is block scoped -- means it will be available in the block (brackets) it is defined.. not a global var
+// }
+// let a=100;
+// console.log(a);
+// }
+
+
+// let a = 20;
+// a = 30;  reassignment of the variable is possible
+
+
+// const a = 25;
+// a = 10;
+
+// console.log(a);
+
+// assignment to constant var is not possible 
+
+
+// <<Variable Naming Convention>>
+
+// 1. no spacing 
+// 2. can use special char, underscore, numbers 
+// 3. CamelCase 
+// 4. meaningful 
+// 5. Secured Keywords
+
+// eg: for Marks of topper use marksOfTopper
+
+
+// <<Datatypes>>
+
+// 1. Primitive Datatype
+// 2. Non-Primitive
+
+//Number, string, boolean, undefined, null, bigint, symbol
+
+// let marks = 9283590723481203498123408132849031283421840182402841824802189401283498123843218401284012803840824018048120841834018401284081208140814082481240824018248242084028402843028342843283440288243482342830248304823048023840320842390489238293402843042380438204832084239;
+
+// console.log(marks);
+// let marks;
+// console.log(typeof(marks));
+
