c381c1b951ce60a9dd629ba70bd689bb6194d947
Prempal <prempal.bt23ece@pec.edu.in>
2025-06-18 12:31:17 +0530

    learinng loops and strings

---
+++ Loops_Strings/index.js
@@ -0,0 +1,40 @@
+// for loop
+
+// for (let i = 0; i <10; i++){
+//     console.log("Peace");
+// }
+
+// for (let i = 5; i > 0; i--){
+//     console.log(i);
+// }
+
+
+// --break (breaks the loop at current iteration)
+
+
+// for (let i = 0; i <10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     else console.log(i);
+// }
+
+
+// --continue (skip current iteration and move on to the next one)
+
+// for (let i = 0; i <10; i++){
+//     if(i == 5){
+//         continue;
+//     }
+//     else console.log(i);
+// }
+
+
+// while loop
+
+
+// let i = 0;
+// while(i <10){
+//     console.log("Peace");
+//     i++;
+// }
