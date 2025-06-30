5cef5a64f742bcc0fb0f3775aeb3f6ced2ee668e
Prempal <prempal.bt23ece@pec.edu.in>
2025-06-30 20:21:28 +0530

    learning functions

---
+++ Functions/index.js
@@ -0,0 +1,48 @@
+// block of code performs a specific function
+
+// function funciton_name("Parameters"){
+//     // code to be executed
+// }
+
+// function saymyname(){
+//     console.log("My name is John Doe");
+// }
+
+// --to use function we need to call it
+// saymyname();
+
+
+// --using parameters in functions
+
+// function printnumber(num){
+//     for (let i =0; i<= num; i++){
+//         console.log(i);
+//     }
+// }
+
+// printnumber(100);
+
+
+// --function to get average of numbers
+
+// function getAverage(num1, num2, num3){
+//     let sum = num1 + num2 + num3;
+//     let average = sum / 3;
+//     console.log("The average is: " , average);
+// }
+
+// getAverage(10, 20, 30);
+
+
+// --return functions
+
+// function getsum(a,b,c){
+//     let sum = a + b + c;
+//     return sum; // returns the value of sum
+// }
+
+// // to get the value returned by the function, we need to store it in a variable
+// let result = getsum(10, 20, 30);
+// console.log("The sum is: ", result);
+
+
