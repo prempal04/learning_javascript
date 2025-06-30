0b71d0747351f9315e62887bdf15994e394aa811
Prempal <prempal.bt23ece@pec.edu.in>
2025-06-30 20:53:28 +0530

    learned Functions

--- Functions/index.js
+++ Functions/index.js
@@ -38,7 +38,7 @@
 
 // function getsum(a,b,c){
 //     let sum = a + b + c;
-//     return sum; // returns the value of sum
+//     return sum; // returns the value of sum , without return it will be undefined
 // }
 
 // // to get the value returned by the function, we need to store it in a variable
@@ -46,3 +46,47 @@
 // console.log("The sum is: ", result);
 
 
+
+// function getMyName(firstName, lastName) {
+//     let fullName = firstName + " " + lastName;
+//     return fullName; // anything written below return will not be executed
+//     // console.log("My name is: ", fullName); // this will not be executed
+// }
+
+// let fullName = getMyName("John", "Doe");
+// console.log(fullName);
+
+
+// -- another example of return function
+
+// function getMultuplication(a,b){
+//     return a*b;
+// }
+
+// console.log(getMultuplication(10, 20)); // 200
+
+
+// --using const and let in functions
+
+// let getMultuplication = function(a, b) {
+//     return a * b;
+// }
+// console.log(getMultuplication(10, 20)); // 200
+
+// using const to define a function
+// const getMultuplication = function(a, b) {
+//     return a * b;
+// }
+// console.log(getMultuplication(10, 20)); // 200
+
+//  it is better to use const for functions as it prevents reassignment
+// if we use let, we can reassign the function to another value
+
+
+// --arrow function
+// let getExp = (x,y) =>{
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2, 3)); // 8
