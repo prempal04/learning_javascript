434fb9252680fc802ce28e314014aa2186d86220
Prempal <prempal.bt23ece@pec.edu.in>
2025-07-03 11:45:51 +0530

    learned in built objects

---
+++ In Built Objects/index.js
@@ -0,0 +1,30 @@
+//Math Object
+
+console.log(Math.PI); // 3.141592653589793
+
+console.log(Math.E); // 2.718281828459045
+
+console.log(Math.SQRT2); // 1.4142135623730951
+
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
+
+console.log(Math.round(1.7930)); // Random number between 0 and 1
+
+console.log(Math.floor(1.7930)); // 1
+
+console.log(Math.ceil(1.7930)); // 1
+
+console.log(Math.abs(-1.7930)); // 1.7930
+
+// Date Object
+
+let currentDate = new Date();
+let date = new Date('July 2025 03 11:38:00')
+let newDate = new Date(2045, 6, 3, 14, 38, 0);
+
+
+console.log(newDate.getDay()); // Current date and time
+
+
