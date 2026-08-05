ab690ebe23f894b664bfe8fc3d69abc0a6ea0f75
Prempal <prempal.bt23ece@pec.edu.in>
2026-08-05 20:18:58 +0530

    Add initial implementation of call stack example with functions, callbacks, and promises; create HTML structure for the application

---
+++ Call Stack/app.js
@@ -0,0 +1,72 @@
+// call stack,  breakpoints & debugging options
+function one() {
+    return 1;
+}
+
+function two() {
+    return one() + one();
+}
+
+function three() {
+    let ans = two() + one();
+    console.log(ans);
+}
+three();
+
+
+
+// JS in single threaded 
+
+let a = 25;
+console.log(a);
+let b = 10;
+console.log(b);
+console.log(a + b);
+
+
+// call backs
+
+setTimeout(function () {
+    console.log("Hello");
+}, 2000);
+
+console.log("done...");
+
+
+// call back hell
+
+
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed!");
+        }, delay);
+    })
+}
+
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("blue", 1000, () => {
+//                 changeColor("purple", 1000, () => {
+//                     changeColor("black", 1000);
+//                 });
+//             });
+//         });
+//     });
+// });
+
+
+// this nesting of callbacks is called callback hell, which is hard to read and maintain. To avoid this, we can use promises and async/await in modern JavaScript.
+
+
+// promises -- these are objects that represent the eventual completion (or failure) of an asynchronous operation and its resulting value. A Promise is in one of these states:
+// pending: initial state, neither fulfilled nor rejected.
+// fulfilled: meaning that the operation completed successfully.
+// rejected: meaning that the operation failed.
+
+
+//  
