9528b550a87d50953aa2fe67ffefe59ccd97537d
Prempal <prempal.bt23ece@pec.edu.in>
2026-08-26 11:16:24 +0530

    module exports: require and import

---
+++ moduleexports/math.js
@@ -0,0 +1,10 @@
+// MODULE EXPORTS -- these are used to export a single value from a module. The value can be a number, string, object, function, etc.
+
+export const sum = (a, b) => a + b;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
+
+
+
+
+// module.exports = 123;
---
+++ moduleexports/script.js
@@ -0,0 +1,13 @@
+import {sum, multiply} from './math.js';
+// let value = require('./math.js');
+
+// console.log(value); // Output: 123
+
+// let fruits = require("./fruits");
+
+// console.log(fruits); // Output: [ { name: 'Apple', color: 'Red' }, { name: 'Banana', color: 'Yellow' }, { name: 'Orange', color: 'Orange' } ]
+
+console.log(sum(2, 3)); // Output: 5
+console.log(multiply(2, 3)); // Output: 6
+
+
